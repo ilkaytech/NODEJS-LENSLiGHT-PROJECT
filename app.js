97eca9e198bbc0b499d65bfd880426fef54fbae5
ilkay <06ilkayozturk@gmail.com>
2023-10-16 15:19:42 +0300

    connect:index

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
-import express from "express";
-
+"use strict";
+/* ---------------
+    Node.JS lenslight project
+-------------------------------- */
+const express = require("express");
 const app = express();
-const port = 8000;
+
+// envVariables to process.env:
+require("dotenv").config();
+const PORT = process.env?.PORT || 8080;
+
+// EJS Templates Engine
+app.set("view engine", "ejs");
+
+// Static Files Middleware
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.send("index sayfası");
+  res.render("index");
+});
+app.get("/about", (req, res) => {
+  res.render("about");
 });
 
-app.listen(port, () => {
-  console.log("Application running on port ${port}");
-});
+// RUN SERVER:
+app.listen(PORT, () => console.log("http://127.0.0.1:" + PORT));
